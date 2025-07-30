11b3cc101c85614470e8345ca424eb827690b9d7
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2025-07-30 14:30:02 +0500

    added channelprovider

---
+++ src/Project.Application/Common/Notifications/Channels/INotificationChannelSenderProvider.cs
@@ -0,0 +1,8 @@
+﻿using Project.Domain.Enums;
+
+namespace Project.Application.Common.Notifications.Channels;
+
+public interface INotificationChannelSenderProvider
+{
+    INotificationChannelSender GetChannel(NotificationChannelType channelType);
+}
---
+++ src/Project.Infrastructure/Common/Notifications/Channels/NotificationChannelSenderProvider.cs
@@ -0,0 +1,12 @@
+﻿using Project.Application.Common.Notifications.Channels;
+using Project.Domain.Enums;
+
+namespace Project.Infrastructure.Common.Notifications.Channels;
+
+public class NotificationChannelSenderProvider : INotificationChannelSenderProvider
+{
+    public INotificationChannelSender GetChannel(NotificationChannelType channelType)
+    {
+        throw new NotImplementedException();
+    }
+}
